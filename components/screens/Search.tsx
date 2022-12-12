2f35698dc73eedf363011161c4ef670c4a0cbd8a
BonesGrocerys <gleb-ilin@list.ru>
2022-12-12 07:00:18 +0300

    Исправленя + Доработки

--- components/screens/Search.tsx
+++ components/screens/Search.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import React, { FC } from "react";
+import { useMusic } from "../../providers/MusicProvider";
+import Album from "../Album";
 
 const Search: FC = () => {
+  const { songs } = useMusic();
   return (
     <View>
-      <Text>search</Text>
+      <ScrollView>
+        <Album />
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  album: {
+    width: 100,
+    height: 100,
+  },
+});
 
 export default Search;
