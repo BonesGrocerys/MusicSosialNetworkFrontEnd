18fa89403da33e280898d232b89ca39df638d957
BonesGrocerys <gleb-ilin@list.ru>
2023-04-10 16:32:17 +0300

    Поиск по трекам

--- components/screens/Search.tsx
+++ components/screens/Search.tsx
@@ -6,27 +6,104 @@ import {
   ScrollView,
   Dimensions,
   TouchableOpacity,
+  Keyboard,
 } from "react-native";
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useMusic } from "../../providers/MusicProvider";
 import Albums from "../Albums";
 import { IMusicians, ITrack } from "../../Interfaces/Tracks";
 import { useNavigation } from "@react-navigation/native";
 import Field from "../ui/Field";
+import axios, { all } from "axios";
+import { IOperationResult } from "../../Interfaces/OperationResult";
+import { API_URL } from "../../providers/api";
+import { Ionicons } from "@expo/vector-icons";
+
 const Search: FC = () => {
-  const statuses = ["Новинки", "Редакция"];
-  const { songsNow, indexNow } = useMusic();
-  const test = [1, 2, 3];
   const navigation = useNavigation();
+  const [allTracks, setAllTracks] = useState<ITrack[]>();
+  const [searchText, setSearchText] = useState("");
   // useEffect(() => {
   //   console.log("TESTTEST");
 
   //   const p = getTracks();
   // }, []);
 
+  const GetAllTracks = async (searchText?: string) => {
+    try {
+      console.log("НАЧАЛО МЕТОДА");
+
+      const { data } = await axios.get<IOperationResult<ITrack[]>>(
+        `${API_URL}/Tracks/get-all-tracks?searchText=${
+          searchText ? searchText : ""
+        }`
+      );
+      if (data.success) {
+        setAllTracks(data.result);
+        return true;
+      }
+    } catch (e) {
+      console.log("ОШИБКА");
+      console.log(e);
+    } finally {
+    }
+  };
+
+  const fetchData = () => {
+    GetAllTracks(searchText);
+  };
+
+  const closeInput = () => {
+    setSearchText("");
+    setAllTracks(undefined);
+    Keyboard.dismiss();
+  };
+
+  useEffect(() => {
+    let debouncer = setTimeout(() => {
+      if (searchText === "") {
+        setAllTracks(undefined);
+      } else {
+        fetchData();
+      }
+    }, 200);
+    return () => {
+      clearTimeout(debouncer);
+    };
+  }, [searchText]);
+
   return (
     <View style={styles.mainContainer}>
-      <View></View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Field
+          onChange={(value) => setSearchText(value)}
+          value={searchText}
+          placeholder="Поиск..."
+          style={{ width: "80%" }}
+          isSearch
+        />
+        <TouchableOpacity onPress={closeInput}>
+          <Ionicons name="md-close-outline" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      {allTracks ? (
+        <View>
+          {allTracks ? <Text style={{ color: "white" }}>Треки:</Text> : <></>}
+          {allTracks?.slice(0, 5).map((track) => (
+            <Text style={{ color: "white" }}> {track.title}</Text>
+          ))}
+        </View>
+      ) : (
+        <View>
+          <Text style={{ color: "white" }}>Не найдено</Text>
+        </View>
+      )}
     </View>
   );
 };
